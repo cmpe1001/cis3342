a05def3fb0005479a4902b5130a57f50e60b7bc1
unknown <tuf01657@CIS-WCC20928.tu.temple.edu>
2014-09-03 10:34:17 -0400

    initial commit for cis3342

---
+++ cis3342solution/WebApplication1/webform01.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webform01.aspx.cs" Inherits="WebApplication1.webform01" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        <asp:TextBox ID="txtInput1" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="dropFunction" runat="server">
+            <asp:ListItem Value="add">+</asp:ListItem>
+            <asp:ListItem Value="subtract">-</asp:ListItem>
+            <asp:ListItem Value="multiply">*</asp:ListItem>
+            <asp:ListItem Value="divide">/</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="txtInput2" runat="server"></asp:TextBox>=<asp:TextBox ID="txtOutput" runat="server"></asp:TextBox>
+        
+        <asp:Button ID="btnCalculate" runat="server" OnClick="Button1_Click" Text="Math it out" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ cis3342solution/WebApplication1/webform01.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1{
+    public partial class webform01 : System.Web.UI.Page{
+        protected void Page_Load(object sender, EventArgs e){
+        }
+
+        protected void Button1_Click(object sender, EventArgs e){
+            double input1 = Convert.ToDouble(txtInput1.Text);
+            double input2 = Convert.ToDouble(txtInput2.Text);
+            string function = dropFunction.SelectedValue;
+            Object output = null;
+
+            calculator calc = new calculator();
+
+            if (function == "add"){
+                output = calc.add(input1, input2);
+            }else if (function == "subtract"){
+                output = calc.subtract(input1, input2);
+            }else if (function == "multiply"){
+                output = calc.multiply(input1, input2);
+            }else if (function == "divide"){
+                output = calc.divide(input1, input2);
+            }
+
+            if (output == null){
+                txtOutput.Text = "Something has gone terribly wrong";
+            }else{
+                txtOutput.Text = Convert.ToString(output);
+            }
+
+        }
+    }
+}
